d37d719ee3c361b89c4bde0dc7dadfb812e06b11
LHB <lhb@LHBdeMacBook-Pro.local>
2016-09-16 22:36:44 +0800

    storyboard分组

--- DYZB_lhb/DYZB_lhb/AppDelegate.swift
+++ DYZB_lhb/DYZB_lhb/AppDelegate.swift
@@ -12,11 +12,12 @@ import UIKit
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
-//    var <#name#> = <#value#>
     
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        
+        UITabBar.appearance().tintColor = UIColor.orange
+        
         return true
     }
 
